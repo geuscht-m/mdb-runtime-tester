9c097c933a371d56345eaaf0d96b1f4b0d78d7a7
Timo Geusch <timo.geusch@mongodb.com>
2019-05-28 16:43:09 -0400

    Mostly done sketching
    Noe adding some tests to confirm some assumptions

--- sketches/prototyping/src/prototyping/helpers.clj
+++ sketches/prototyping/src/prototyping/helpers.clj
@@ -1,24 +1,62 @@
 ;; Helper functions for Mongodb discovery mechanisms
 
 (in-ns 'prototyping.core)
+(require '[clojure.string :as str])
 
+;;
+;; A bunch of mongodb driver interface helpers
+;;
+(defn- run-listshards
+  "Returns the output of the mongodb listShards admin command"
+  [uri]
+  )
+
+(defn- run-replset-get-config
+  "Returns the output of mongodb's replSetGetConfig admin command"
+  [uri]
+  )
+
+(defn- run-replset-get-status
+  "Returns the result of Mongodb's replSetGetStatus admin command"
+  [uri]
+  )
+
+(defn- run-get-shard-map
+  "Returns the output of MongoDB's getShardMap admin command"
+  [uri]
+  )
+
+(defn- run-replset-stepdown
+  [uri]
+  )
+
+(defn- run-shutdown-command
+  "Run the shutdown command on a remote or local mongod/s"
+  [uri & force]
+  )
+
+(defn- run-remote-ssh-command
+  [cmd]
+  )
 ;; Replica set topology functions to
 ;; - Retrieve the connection URI for the primary/secondaries
 ;; - Get the number of nodes in an RS
 (defn get-rs-primary
   "Retrieve the primary from a given replica set. Fails if URI doesn't point to a valid replica set"
   [uri]
-  "")
+  (let [rs-state (run-replset-get-status uri)]
+    (first (filter #(= (get :stateStr %) "PRIMARY") (get :members rs-state)))))
 
 (defn get-rs-secondaries
   "Retrieve a list of secondaries for a given replica set. Fails if URI doesn't point to a valid replica set"
   [uri]
-  ())
+  (let [rs-state (run-replset-get-status uri)]
+    (filter #(= (get :stateStr %) "SECONDARY") (get :members rs-state))))
 
 (defn get-num-rs-members
   "Retrieve the number of members in a replica set referenced by its uri"
   [uri]
-  0)
+  (count (run-replset-get-status uri)))
 
 
 (defn is-local-process?
@@ -35,39 +73,44 @@
   ())
 
 (defn start-remote-mongo-process [uri]
-  ())
+  (run-remote-ssh-command uri))
 
 (defn stop-mongod-process [uri]
-  ())
+  (run-shutdown-command uri))
 
 (defn stop-mongos-process [uri]
-  ())
+  (run-shutdown-command uri))
 
 
 (defn send-mongo-rs-stepdown
   "Sends stepdown to the mongod referenced by the URI"
   [uri]
-  )
+  (run-replset-stepdown uri))
   
 (defn get-random-members
   "Returns a list of n random replica set members from the replica set referenced by uri"
   [uri n]
-  )
+  (let [rs-members (get :members (run-replset-get-status uri))]
+    (take n (shuffle rs-members))))
 
 (defn get-random-shards
   "Returns a list of n random shards from the sharded cluster referenced by the uri"
   [uri n]
-  )
+  (let [shards (get :shards (run-listshards uri))]
+    (take n (shuffle shards))))
 
 (defn get-config-servers-uri
   "Given a sharded cluster, returns the URI needed to connect to the config servers"
   [cluster-uri]
-  )
+  (let [shard-map (run-get-shard-map cluster-uri)]
+    (str/split (get :config (get :map shard-map)) #",")))
 
 (defn get-shard-uris
-  "Retrieve the URIs for the individual shards that make up the sharded cluster"
+  "Retrieve the URIs for the individual shards that make up the sharded cluster.
+   cluster-uri _must_ point to the mongos for correct discovery."
   [cluster-uri]
-  )
+  (let [shard-configs (run-listshards cluster-uri)]
+    (map (get :host) (get :shards shard-configs))))
 
 (defn not-expired?
   "Check if the current time is still within the expected interval"
@@ -77,7 +120,7 @@
 (defn is-sharded-cluster?
   "Check if the cluster specified by the URI is a sharded cluster or a replica set"
   [uri]
-  )
+  (empty? (get-shard-uris uri)))
 
 (defn undo-operation
   "On functions that return a closure, execute the closure"
--- sketches/prototyping/test/prototyping/core_test.clj
+++ sketches/prototyping/test/prototyping/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [prototyping.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
+(deftest test-config-servers-uri
+  (testing "Checking if we get the correct list of config servers"
+    (is (= (get-config-servers-uri "mongodb://localhost:27017") nil))))
+
+(deftest test-shard-uris
+  (testing "Correct retrieval of shard member uris"
     (is (= 0 1))))
+
+(deftest test-rs-member-retrieval
+  (testing "Make sure we get all the info about the replica set members"
+    (is (= 0 9))))
