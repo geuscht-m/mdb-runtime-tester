cc4fe2ced3cc504b57169786446c710dd927a763
Timo Geusch <timo.geusch@mongodb.com>
2019-11-18 17:18:24 -0500

    Cleaned up unused code

--- sketches/prototyping/src/prototyping/core.clj
+++ sketches/prototyping/src/prototyping/core.clj
@@ -1,11 +1,6 @@
 (ns prototyping.core
   (:gen-class :main true))
 
-;; (defn -main
-;;   "I don't do a whole lot ... yet."
-;;   [& args]
-;;   (println "Hello, World!"))
-
 (load "helpers")
 (load "basic-functions")
 (load "test-functions")
