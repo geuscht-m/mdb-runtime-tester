8725fd81778c7175f064db5a69d4a4ef64f55d7e
Timo Geusch <timo.geusch@mongodb.com>
2019-11-18 16:30:08 -0500

    Added process listing functionality

---
+++ sketches/prototyping/src/prototyping/sys_helpers.clj
@@ -0,0 +1,33 @@
+(ns prototyping.sys-helpers
+  (:require [clojure.java.shell :refer [sh]]))
+
+(defn get-os-type
+  "Retrieve the OS of the current system"
+  []
+  (System/getProperty "os.name"))
+
+(defn- parse-ps-output
+  "Parse a line of 'ps ax' output"
+  [ps-line]
+  (let [parsed-line (re-find (re-matcher #"^(\d+)\s+(.+)" (clojure.string/triml ps-line)))]
+    (println parsed-line)
+    { :pid (nth parsed-line 1) :command-line (nth parsed-line 2) }))
+
+(defn- get-process-list-windows
+  []
+  )
+
+(defn- get-process-list-bsd
+  []
+  (let [proc-list (sh "ps" "ax" "-o" "pid=" "-o" "command=")]
+    (doall (map #(parse-ps-output %) (clojure.string/split-lines (get proc-list :out))))))
+
+;; NOTE - this code can be improved by using the ProcessHandle API on Java 9+
+;;        As it is written, this code also works with Java 8.
+(defn get-process-list
+  []
+  (let [os (get-os-type)]
+    (cond
+        (= os "Linux") (get-process-list-bsd)
+        (= os "Darwin") (get-process-list-bsd)
+        (= os "Windows") (get-process-list-windows))))
---
+++ sketches/prototyping/test/prototyping/sys_helpers_test.clj
@@ -0,0 +1,9 @@
+(ns prototyping.sys-helpers-test
+  (:require [prototyping.sys-helpers :refer :all]
+            [clojure.test :refer :all]
+            [clojure.pprint :refer :all]))
+
+(deftest test-get-process-list
+  (testing "Check that we can get the list of processes"
+                                        ;(doall (map #(pprint %) get-process-list))))
+    (pprint (get-process-list))))
