45cf9ec34b91efef203ebd0bd686c2c011daa668
Timo Geusch <timo.geusch@mongodb.com>
2020-01-08 11:29:25 -0500

    Fixed bind_ip issue

--- ssh-test-setup/start-mongod-replset-node.sh
+++ ssh-test-setup/start-mongod-replset-node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p $HOME/data
-mongod --dbpath $HOME/data --fork --replSet test --logpath $HOME/data/mongod.log
+mongod --dbpath $HOME/data --fork --replSet replTest --logpath $HOME/data/mongod.log --bind_ip_all
