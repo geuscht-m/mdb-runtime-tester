c5ae700e9cf733722508555a0fe108397079eb2f
Timo Geusch <timo.geusch@mongodb.com>
2020-03-11 11:02:23 -0400

    Updated test creations scripts to set reduced timeouts
    Timeout settings changed to much lower limits to improve test
    responsiveness.

--- prototyping/set-up-test-env.sh
+++ prototyping/set-up-test-env.sh
@@ -19,7 +19,11 @@ mlaunch init --single --dir $HOME/tmp/mdb-test/single
 mlaunch stop --dir $HOME/tmp/mdb-test/single
 
 mlaunch init --replicaset --nodes 5 --dir $HOME/tmp/mdb-test/replica
+mongo "mongodb://localhost/?replicaSet=replset" config-timeouts.js
 mlaunch stop --dir $HOME/tmp/mdb-test/replica
 
 mlaunch init --replicaset --nodes 3 --shards 3 --config 3 --dir $HOME/tmp/mdb-test/sharded
+mongo "mongodb://localhost:27018,localhost:27019,localhost:27020/?replicaSet=shard01" config-timeouts.js
+mongo "mongodb://localhost:27021,localhost:27022,localhost:27023/?replicaSet=shard02" config-timeouts.js
+mongo "mongodb://localhost:27024,localhost:27025,localhost:27026/?replicaSet=shard03" config-timeouts.js
 mlaunch stop --dir $HOME/tmp/mdb-test/sharded
