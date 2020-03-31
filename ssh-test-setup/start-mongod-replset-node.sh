1707d9380ff72c78b9d5f6754d43fd887d543404
Timo Geusch <timo.geusch@mongodb.com>
2020-03-31 15:18:54 -0400

    Updated to correctly configure the test replicasets on rs1-3

--- ssh-test-setup/start-mongod-replset-node.sh
+++ ssh-test-setup/start-mongod-replset-node.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-#mkdir -p $HOME/data
-#mongod --dbpath $HOME/data --fork --replSet replTest --logpath $HOME/data/mongod.log --bind_ip_all --auth --keyFile $HOME/data/keyfile
 mongod -f mongod-ssh-rs.conf
 mongod -f mongod-ssh-ssl.conf
