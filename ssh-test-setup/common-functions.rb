4e0105c77adbcd78ec3dac9a9f0b454f2e454369
Timo Geusch <timo.geusch@mongodb.com>
2020-08-13 17:14:20 -0400

    Factored out some common code between CentOS and Ubuntu vagrant VMs

---
+++ ssh-test-setup/common-functions.rb
@@ -0,0 +1,90 @@
+require 'fileutils'
+
+module CommonSetupFunctions
+  
+  def self.createBaseDirs(max_rs)
+    if not File.directory? 'tls'
+      Dir.mkdir 'tls'
+    end
+    if not File.directory? 'tls/CA'
+      Dir.mkdir 'tls/CA'
+    end
+    if not File.directory? 'tls/testdriver'
+      Dir.mkdir 'tls/testdriver'
+    end
+    if not File.directory? 'tls/user-cert'
+      Dir.mkdir 'tls/user-cert'
+    end
+    (1..max_rs).each do |i|
+      if not File.directory? "tls/rs#{i}"
+        Dir.mkdir "tls/rs#{i}"
+      end
+    end
+  end
+
+  def self.createTestServerCert(server_num)
+    tmp_cnf = "[SAN]\nsubjectAltName = @altNames\n[altNames]\nDNS.1 = localhost\nDNS.2 = rs#{server_num}.mongodb.test\n"
+    tmp_cnf_name = "ext_openssl_rs#{server_num}.cnf"
+    File.write(tmp_cnf_name, tmp_cnf)
+    system("openssl req -config /etc/ssl/openssl.cnf -new -nodes -keyout tls/rs#{server_num}/rs#{server_num}.key -out tls/rs#{server_num}/rs#{server_num}.csr -days 365 -subj \"/C=US/ST=NY/L=NYC/O=TEST/OU=Server/CN=rs#{server_num}.mongodb.test\"")
+    system("openssl x509 -req -days 365 -sha256 -in tls/rs#{server_num}/rs#{server_num}.csr -CA tls/CA/root.crt -CAkey tls/CA/root.key -CAcreateserial -extensions SAN -extfile ext_openssl_rs#{server_num}.cnf -out tls/rs#{server_num}/rs#{server_num}.crt")
+    system("cat tls/rs#{server_num}/rs#{server_num}.crt tls/rs#{server_num}/rs#{server_num}.key > tls/rs#{server_num}/rs#{server_num}.pem")
+    File.delete(tmp_cnf_name)
+  end
+
+  def self.createTestDriverCert
+    tmp_cnf = "[SAN]\nsubjectAltName = @altNames\n[altNames]\nDNS.1 = localhost\nDNS.2 = testdriver.mongodb.test\n"
+    File.write('ext_openssl_td.cnf', tmp_cnf)
+    system('openssl req -config /etc/ssl/openssl.cnf -new -nodes -keyout tls/testdriver/testdriver.key -out tls/testdriver/testdriver.csr -days 365 -subj "/C=US/ST=NY/L=NYC/O=TEST/OU=Server/CN=testdriver.mongodb.test"')
+    system('openssl x509 -req -days 365 -sha256 -in tls/testdriver/testdriver.csr -CA tls/CA/root.crt -CAkey tls/CA/root.key -CAcreateserial -out tls/testdriver/testdriver.crt')
+    system('cat tls/testdriver/testdriver.crt tls/testdriver/testdriver.key > tls/testdriver/testdriver.pem')
+    File.delete('ext_openssl_td.cnf')
+  end
+
+  def self.createUserAuthCert
+    system("openssl req -config /etc/ssl/openssl.cnf -new -nodes -keyout tls/user-cert/user-cert.key -out tls/user-cert/user-cert.csr -days 365 -subj \"/C=US/ST=NY/L=NYC/O=TEST/OU=Users/CN=test-user\"")
+    #system("openssl x509 -req -days 365 -sha256 -in tls/user-cert/user-cert.csr -CA tls/CA/root.crt -CAkey tls/CA/root.key -CAcreateserial -extensions SAN -extfile ext_openssl_user-test.cnf -out tls/user-test/user-test.crt")
+    system("openssl x509 -req -days 365 -sha256 -in tls/user-cert/user-cert.csr -CA tls/CA/root.crt -CAkey tls/CA/root.key -CAcreateserial -out tls/user-cert/user-cert.crt")
+    system("cat tls/user-cert/user-cert.crt tls/user-cert/user-cert.key > tls/user-cert/user-cert.pem")
+  end
+
+  def self.cleanUpCerts
+    system('rm tls/user-cert/*')
+    system('rm tls/CA/*')
+    system('rm tls/testdriver/*')
+    system('rm tls/rs?/*')
+  end
+
+
+  def self.setupTLSCerts(operation, num_test_servers)
+    if operation == 'up' || operation == 'provision'
+      createBaseDirs(num_test_servers)
+      # Generate root certificate
+      system('openssl req -x509 -config /etc/ssl/openssl.cnf -new -nodes -keyout tls/CA/root.key -sha256 -days 365 -out tls/CA/root.crt -subj "/C=US/ST=NY/L=NYC/O=TEST/CN=auth.mongodb.test"')
+      # Generate certificates for the replica set members
+      (1..num_test_servers).each do |i|
+        createTestServerCert(i)
+      end
+      createTestDriverCert
+      createUserAuthCert
+    elsif operation == 'destroy'
+      cleanUpCerts
+    end
+  end
+
+  def self.setupSSHKeys(operation)
+    insecure_ssh_pub_key = ''
+    if operation == 'up' || operation == 'provision'
+      if File.directory?("ssh")
+        FileUtils.rm_rf('ssh')
+      end
+      Dir.mkdir('ssh')
+      system('ssh-keygen -t ed25519 -f ssh/id_ed25519 -N ""')
+      insecure_ssh_pub_key = File.readlines('ssh/id_ed25519.pub').first.strip
+    elsif operation == 'destroy'
+      FileUtils.rm_rf('ssh')
+    end
+    return insecure_ssh_pub_key
+  end
+
+end
