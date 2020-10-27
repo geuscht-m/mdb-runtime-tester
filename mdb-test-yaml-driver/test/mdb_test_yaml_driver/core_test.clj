549567c47d66e12f529c933c3e538c47632563ba
Timo Geusch <timo.geusch@mongodb.com>
2020-10-27 15:22:01 -0400

    Fixed map/parameter merge issues
    - Most parameter maps should now work OK
    - Started expanding test coverage
    - Added mock recipient function that just reflects the parameters
      back.

--- mdb-test-yaml-driver/test/mdb_test_yaml_driver/core_test.clj
+++ mdb-test-yaml-driver/test/mdb_test_yaml_driver/core_test.clj
@@ -5,7 +5,7 @@
 
 (deftest parse-test-settings-empty-test
   (testing "Check that the test setting parser generates the correct sparse map for"
-    (let [config-map {}
+    (let [config-map (clojure.lang.PersistentHashMap/EMPTY)
           result-map (impl/parse-test-settings config-map)]
       (is (= (count result-map) 0)))))
 
@@ -37,3 +37,34 @@
       (is (not (nil? parsed-file)))
       (is (= 4 (count parsed-file)))
       (is (contains? (first parsed-file) :Config)))))
+
+(defn- mock-trigger-election
+  [rs-uri & { :keys [forced user pwd ssl root-ca client-cert auth-mechanism] :or {forced false user nil pwd nil ssl false root-ca nil client-cert nil auth-mechanism nil}}]
+  ;;(println "Calling mock-trigger-election")
+  (let [results { :rs-uri rs-uri :forced forced :user user :pwd pwd :ssl ssl :root-ca root-ca :client-cert client-cert :auth-mechanism auth-mechanism }]
+    ;;(println "mock-trigger-election results are " results)
+    results))
+
+(deftest basic-stepdown-test
+  (testing "Check that we correctly parse trigger-election call and pass in the correct parameters"
+    (is (.exists (io/file "resources/trigger-test.yaml")))
+    (with-redefs-fn {#'tester-core.core/trigger-election mock-trigger-election}
+      #(let [parsed-file (impl/parse         (list "resources/trigger-test.yaml"))
+             results     (impl/execute-tests parsed-file)]
+         ;; (println "parsed-file is " parsed-file)
+         ;;(println "basic-stepdown-test results are " results)
+         (is (= 1 (count results)))
+         (is (= "mongodb://localhost:27017,localhost:27018,localhost:27019/" (:rs-uri (first results))))))))
+
+(deftest basic-stepdown-test-with-config
+  (testing "Check that we correctly parse trigger-election call and pass in the correct parameters, this time with SSL enabled in the Config section"
+    (is (.exists (io/file "resources/trigger-test-with-config.yaml")))
+    (with-redefs-fn {#'tester-core.core/trigger-election mock-trigger-election}
+      #(let [parsed-file (impl/parse         (list "resources/trigger-test-with-config.yaml"))
+             results     (impl/execute-tests parsed-file)]
+         ;; (println "parsed-file is " parsed-file)
+         ;;(println "basic-stepdown-test-with-config results are " results)
+         (is (= 1 (count results)))
+         (is (= "mongodb://localhost:27017,localhost:27018,localhost:27019/" (:rs-uri (first results))))
+         (is (= true (:ssl (first results))))))))
+
