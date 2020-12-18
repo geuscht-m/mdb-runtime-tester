a8f0271378d4be6d11c04150d7fa57daa07c8f18
Timo Geusch <timo.geusch@mongodb.com>
2020-12-18 16:15:04 -0500

    Added positive and negative service tests

--- tester-core/test/tester_core/sys_helpers_test.clj
+++ tester-core/test/tester_core/sys_helpers_test.clj
@@ -1,5 +1,6 @@
 (ns tester-core.sys-helpers-test
   (:require [tester-core.sys-helpers :refer :all]
+            [tester-core.test-helpers :refer [ssh-apply-command-to-rs-servers]]
             [tester-core.core :refer [run-server-status]]
             [clojure.test :refer :all]
             [clojure.pprint :refer :all]))
@@ -20,12 +21,22 @@
 
 (deftest test-get-os-type-remote
   (testing "Check the result of remote get-os-type"
-    (let [os-type (get-os-type "rs1")]
+    (let [os-type (get-os-type "rs1.mongodb.test")]
       (is (= os-type "Linux")))))
 
-(deftest test-is-service
+(deftest test-is-not-service
   (testing "Check if remote MongoDB was started as a service or not"
+    (ssh-apply-command-to-rs-servers "./start-mongod-replset-node.sh" (list "rs1.mongodb.test"))
     (let [pid (:pid (run-server-status "mongodb://rs1.mongodb.test:27017" :user "admin" :pwd "pw99"))
           result (check-if-service "rs1.mongodb.test" pid)]
-      (is (= result false)))))
-      
+      (is (= result false)))
+    (ssh-apply-command-to-rs-servers "pkill mongod" (list "rs1.mongodb.test"))))
+
+
+(deftest test-is-service
+  (testing "Make sure we correcty identify a mongod that was started as a service"
+    (ssh-apply-command-to-rs-servers "sudo systemctl start mongod" (list "rs1.mongodb.test"))
+    (let [pid (:pid (run-server-status "mongodb://rs1.mongodb.test:27107"))
+          result (check-if-service "rs1.mongodb.test" pid)]
+      (is (= result true)))
+    (ssh-apply-command-to-rs-servers "sudo systemctl stop mongod" (list "rs1.mongodb.test"))))
