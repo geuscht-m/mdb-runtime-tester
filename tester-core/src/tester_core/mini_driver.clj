c8f1f6bf9b0f17cc280033be7def86d8fcae36ce
Timo H. Geusch <timo-git@unix-consult.com>
2021-03-05 14:05:01 -0500

    Cleaned up main mdb-connect function

--- tester-core/src/tester_core/mini_driver.clj
+++ tester-core/src/tester_core/mini_driver.clj
@@ -94,19 +94,22 @@
 
 (defn- create-ssl-mongo-client-scram-no-ssl-context
   [mongo-uri ssl user pwd]
-  (let [settings (ConnectionString. mongo-uri)
-        cred     (MongoCredential/createCredential user "admin" (char-array pwd))]
-    ;;(println "Creating SCRAM connection to " mongo-uri " for user " user " with default SSL context")
-    (MongoClients/create
-     (-> (MongoClientSettings/builder)
-         (.applyConnectionString (ConnectionString. mongo-uri))                                          
-         (.applyToSslSettings(reify
-                               com.mongodb.Block
-                               (apply [this s] (-> s
-                                                   (.enabled ssl)
-                                                   (.build)))))
-         (.credential cred)
-         (.build)))))
+  (let [settings (ConnectionString. mongo-uri)]
+    (if (nil? user)
+      (do (println "Creating unauthenticated connection to uri " mongo-uri)
+          (MongoClients/create settings))
+      (let cred     [(MongoCredential/createCredential user "admin" (char-array pwd))]
+           ;;(println "Creating SCRAM connection to " mongo-uri " for user " user " with default SSL context")
+           (MongoClients/create
+            (-> (MongoClientSettings/builder)
+                (.applyConnectionString (ConnectionString. mongo-uri))                                          
+                (.applyToSslSettings(reify
+                                      com.mongodb.Block
+                                      (apply [this s] (-> s
+                                                          (.enabled ssl)
+                                                          (.build)))))
+                (.credential cred)
+                (.build)))))))
 
 (defn- create-ssl-mongo-client-scram-with-ssl-context
   [mongo-uri ssl user pwd root-ca]
@@ -172,14 +175,9 @@
   (timbre/debug "Attempting to connect to " mongo-uri)
   ;;(println "Trying to connect to " mongo-uri " with user " user ", password", pwd ", ssl " ssl " and root-ca " root-ca)
   (if (or (nil? auth-mechanism) (str/starts-with? auth-mechanism "SCRAM-SHA"))
-    ;; Connect either without user information, or all of the authentication information
-    ;; encoded in the URI connection string
-    (let [ssl-enabled (or ssl (.contains mongo-uri "ssl=true"))]
-      (cond (nil? user) (if ssl-enabled
-                          (create-client-with-ssl mongo-uri ssl-enabled root-ca)
-                          (MongoClients/create (ConnectionString. mongo-uri)))
-            (and (not (nil? user))
-                 (not (nil? pwd))) (create-scram-client-with-ssl mongo-uri user pwd ssl-enabled root-ca)))
+    (if (nil? root-ca)
+      (create-ssl-mongo-client-scram-no-ssl-context mongo-uri (or ssl (.contains mongo-uri "ssl=true")) user pwd)
+      (create-ssl-mongo-client-scram-with-ssl-context mongo-uri (or ssl (.contains mongo-uri "ssl=true")) user pwd root-ca)
     (cond (and (= auth-mechanism :mongodb-x509) (nil? user))
           (create-x509-client-with-ssl-and-client-cert mongo-uri root-ca client-cert)
           (and (= auth-mechanism :mongodb-x509)
