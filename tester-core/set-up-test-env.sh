3aaf39c269c3734ec79a92d733d3632aaeb5e9d4
Timo Geusch <timo.geusch@mongodb.com>
2021-03-11 20:19:47 -0500

    Improved Vagrant setup for testing

--- tester-core/set-up-test-env.sh
+++ tester-core/set-up-test-env.sh
@@ -11,24 +11,26 @@
 #
 rm -rf $HOME/tmp/mdb-test
 
-mkdir -p $HOME/tmp/mdb-test/single
+#mkdir -p $HOME/tmp/mdb-test/single
 mkdir -p $HOME/tmp/mdb-test/replica
 mkdir -p $HOME/tmp/mdb-test/sharded
 
-mlaunch init --single --dir $HOME/tmp/mdb-test/single
-mlaunch stop --dir $HOME/tmp/mdb-test/single
-sleep 5s
+# mlaunch init --single --dir $HOME/tmp/mdb-test/single
+# mlaunch stop --dir $HOME/tmp/mdb-test/single
+# sleep 5s
 
 mlaunch init --replicaset --nodes 5 --dir $HOME/tmp/mdb-test/replica --wiredTigerCacheSizeGB 0.1
 sleep 10s
 mongo "mongodb://localhost/?replicaSet=replset" config-timeouts.js
+sleep 5s
 mlaunch stop --dir $HOME/tmp/mdb-test/replica
 
-sleep 5s
+sleep 10s
 
 mlaunch init --replicaset --nodes 3 --shards 3 --config 3 --dir $HOME/tmp/mdb-test/sharded --wiredTigerCacheSizeGB 0.1
 sleep 30s
 mongo "mongodb://localhost:27018,localhost:27019,localhost:27020/?replicaSet=shard01" config-timeouts.js
 mongo "mongodb://localhost:27021,localhost:27022,localhost:27023/?replicaSet=shard02" config-timeouts.js
 mongo "mongodb://localhost:27024,localhost:27025,localhost:27026/?replicaSet=shard03" config-timeouts.js
+sleep 5s
 mlaunch stop --dir $HOME/tmp/mdb-test/sharded
